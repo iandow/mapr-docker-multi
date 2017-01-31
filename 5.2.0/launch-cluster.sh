99b89a199ae78a82029a82a6c589f213413a2557
iandow <kayaking@gmail.com>
2017-01-31 11:29:05 -0800

    fix problems with zkdata, /etc/hosts, and CLDB

--- 5.2.0/launch-cluster.sh
+++ 5.2.0/launch-cluster.sh
@@ -1,56 +1,29 @@
 #!/bin/bash
 
-# Compare dot seperated versions
-# reference: http://stackoverflow.com/questions/4023830/how-compare-two-strings-in-dot-separated-version-format-in-bash
-vercomp () {
-    if [[ $1 == $2 ]]
-    then
-        return 0
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            return 1
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            return 2
-        fi
-    done
-    return 0
-}
-
 MAPRVER="5.2.0"
 # Docker Checks
 if [[ -z $(which docker)  ]] ; then
-        echo " docker could not be found on this server. Please install Docker version 1.6.0 or later."
+	echo " docker could not be found on this server. Please install Docker version 1.6.0 or later."
 	echo " If it is already installed Please update the PATH env variable." 
-        exit
+	exit
 fi
 
 dv=$(docker --version | awk '{ print $3}' | sed 's/,//')
-vercomp $dv 1.6.0
-if [[ $? = 2 ]] ; then
-        echo " Docker version installed on this server : $dv.  Please install Docker version 1.6.0 or later."
-        exit
+if [[ $dv < 1.06.0 ]] ; then
+	echo " Docker version installed on this server : $dv.  Please install Docker version 1.6.0 or later."
+	exit
+fi
+
+# sshpass Checks
+if [[ -z $(which sshpass)  ]] ; then
+	echo " sshpass could not be found on this server. Please install sshpass."
+	echo " If it is already installed Please update the PATH env variable." 
+	exit
 fi
 
 # Usage Check.
 if [[ $# -ne 4 ]]
-then
+	then
 	echo " Usage : $0 ClusterName NumberOfNodes MemSize-in-kB Path-to-DisklistFile"
 	exit
 fi
@@ -61,7 +34,7 @@ MEMTOTAL=$3
 DISKLISTFILE=$4
 
 if [[ ! -f ${DISKLISTFILE} ]]
-then
+	then
 	echo " Disklistile : ${DISKLISTFILE} doesn't exist"
 	exit
 fi
@@ -71,7 +44,7 @@ fi
 declare -a disks=(`cat ${DISKLISTFILE}`)
 
 if [[ ${#disks[@]} -eq 0 ]] 
-then
+	then
 	echo "There are no usable disks on this server."
 	exit
 fi
@@ -99,9 +72,10 @@ if [[ ${NUMBEROFNODES} -lt ${#disks[@]} ]] ; then
 	cldbdisks=$(join , ${disks[0]} ${disks[@]:$NUMBEROFNODES})
 fi
 
+# overlay /opt/mapr/zkdata in order to workaround a wierd permissions error
+sudo rm -rf /tmp/zkdata
 mkdir -p /tmp/zkdata:/opt/mapr/zkdata
-
-cldb_cid=$(docker run -d --privileged -h ${CLUSTERNAME}c1 -v /tmp/zkdata:/opt/mapr/zkdata -e "DISKLIST=$cldbdisks" -e "CLUSTERNAME=${CLUSTERNAME}" -e "MEMTOTAL=${MEMTOTAL}" docker.io/maprtech/mapr-control-cent67:${MAPRVER})
+cldb_cid=$(docker run -d --privileged -h ${CLUSTERNAME}c1 -v /tmp/zkdata:/opt/mapr/zkdata -e "DISKLIST=$cldbdisks" -e "CLUSTERNAME=${CLUSTERNAME}" -e "MEMTOTAL=${MEMTOTAL}" -p 9443:8443 -p 9422:22 docker.io/maprtech/mapr-control-cent67:${MAPRVER})
 container_ids[0]=$cldb_cid
 
 sleep 10
@@ -113,27 +87,25 @@ sleep 20
 # Launch Data Nodes 
 i=1
 while [[ $i -lt $NUMBEROFNODES ]]
-do
-  data_cid=$(docker run -d --privileged -h ${CLUSTERNAME}d${i} -e "CLDBIP=${cldbip}" -e "DISKLIST=${disks[$i]}" -e "CLUSTERNAME=${CLUSTERNAME}" -e "MEMTOTAL=${MEMTOTAL}" docker.io/maprtech/mapr-data-cent67:${MAPRVER})
-  container_ids[$i]=$data_cid
-  sleep 10
-  dip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${data_cid} )
-  container_ips[$i]=$dip
-  echo -e "$dip\t${CLUSTERNAME}d${i}.mapr.io\t${CLUSTERNAME}d${i}" >> /tmp/hosts.$$
-  i=`expr $i + 1`
+	do
+	data_cid=$(docker run -d --privileged -h ${CLUSTERNAME}d${i} -e "CLDBIP=${cldbip}" -e "DISKLIST=${disks[$i]}" -e "CLUSTERNAME=${CLUSTERNAME}" -e "MEMTOTAL=${MEMTOTAL}" docker.io/maprtech/mapr-data-cent67:${MAPRVER})
+	container_ids[$i]=$data_cid
+	sleep 10
+	dip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${data_cid} )
+	container_ips[$i]=$dip
+	echo -e "$dip\t${CLUSTERNAME}d${i}.mapr.io\t${CLUSTERNAME}d${i}" >> /tmp/hosts.$$
+	i=`expr $i + 1`
 done
 
 
 #Populate the /etc/hosts on all the nodes
 for ip in "${container_ips[@]}"
-do
+	do
 	sshpass -p "mapr" scp -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r /tmp/hosts.$$ ${ip}:/tmp/hosts
-	sshpass -p "mapr" ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${ip} 'cat /tmp/hosts >> /etc/hosts'
+	sshpass -p "mapr" ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${ip} 'cat /tmp/hosts >> /etc/hosts'
 done
-
-# For Spark
-sleep 60
-sshpass -p "mapr" ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${cldbip} 'hadoop fs -mkdir /apps/spark; hadoop fs -chmod 777 /apps/spark'
 
 echo -n "Data Nodes : "
 join , ${container_ips[@]:1}
+
+rm /tmp/hosts.$$
